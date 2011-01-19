08ca3e046d0a3c52aea4d150abd34290ba52f6a2
amiele <amiele>
2011-01-19 13:53:38 +0000

    *** empty log message ***

--- _projectCommon.php
+++ _projectCommon.php
@@ -53,17 +53,6 @@
 	$App->Promotion = TRUE;
 	
 	# If you have Google Analytics code, use it here
-	 $App->SetGoogleAnalyticsTrackingCode("
-	 <script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-7607153-2']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-   									 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  										})();</script>
-  										");
+	 $App->SetGoogleAnalyticsTrackingCode("UA-7607153-2");
+	 
 ?>
