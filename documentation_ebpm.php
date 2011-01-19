d099432fe24bac93b514a149ddff50bb3658f8da
amiele <amiele>
2011-01-19 12:25:01 +0000

    *** empty log message ***

--- documentation_ebpm.php
+++ documentation_ebpm.php
@@ -15,19 +15,7 @@
 
 	$html  = <<<EOHTML
 <div id="midcolumn">
-<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-7607153-3']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 <h2>$pageTitle</h2>
 <p></p>
 
